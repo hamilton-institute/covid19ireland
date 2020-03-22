22f30a9824fe40827acd5057cabf6f1ba59b06aa
Trent Rogers <tarogers479@gmail.com>
2020-03-22 11:37:05 +0000

    Put a horrific looking layout in place.  Saving my progress now before I switch everything over to shinydashboard.

--- shiny/covid19_hamilton/ui.R
+++ shiny/covid19_hamilton/ui.R
@@ -12,54 +12,6 @@ library(tidyverse)
 library(plotly)
 library(leaflet)
 
-body <- dashboardBody(
-  fluidRow(
-    box(
-      title = "Box title", width = 6, status = "primary",
-      "Box content"
-    ),
-    box(
-      status = "warning", width = 6,
-      "Box content"
-    )
-  ),
-    
-  fluidRow(
-    column(width = 4,
-      box(
-        title = "Title 1", width = NULL, solidHeader = TRUE, status = "primary",
-        "Box content"
-      ),
-      box(
-        width = NULL, background = "black",
-        "A box with a solid black background"
-      )
-    ),
-
-    column(width = 4,
-      box(
-        title = "Title 3", width = NULL, solidHeader = TRUE, status = "warning",
-        "Box content"
-      ),
-      box(
-        title = "Title 5", width = NULL, background = "light-blue",
-        "A box with a solid light-blue background"
-      )
-    ),
-
-    column(width = 4,
-      box(
-        title = "Title 2", width = NULL, solidHeader = TRUE,
-        "Box content"
-      ),
-      box(
-        title = "Title 6", width = NULL, background = "maroon",
-        "A box with a solid maroon background"
-      )
-    )
-  )
-)
-
 # Define UI for application that draws a histogram
 shinyUI(
     fluidPage(
