fab234b67e828261d082dca2d0489801bef814f3
Danilo Sarti <danilosarti@Danilos-MacBook-Pro.local>
2020-03-24 12:01:58 +0000

    first commit to push the hdi index data

---
+++ data/short script to write HDI on RDS file.R
@@ -0,0 +1,9 @@
+#####Human Development Index Data from http://hdr.undp.org/en/data# 
+
+### for saving the data on RDA format
+
+load(file = "/Users/danilosarti/Downloads/hdis.RData")
+saveRDS(hdis,file = "~/hamilton-monitor/data/hdi.rds")
+
+
+
