55cef581d1df2b61a68e8f9c5626fb62331bff17
Andrew Parnell <andrew.parnell@ucd.ie>
2020-05-13 08:08:53 +0100

    ECDC changed scrape format

--- shiny/covid19_hamilton/scrape_scripts/ECDC_scrape.R
+++ shiny/covid19_hamilton/scrape_scripts/ECDC_scrape.R
@@ -4,15 +4,22 @@
 library(readxl)
 library(httr)
 library(tidyverse)
+library(utils)
 
 #create the URL where the dataset is stored with automatic updates every day
+# url <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",format(Sys.time(), "%Y-%m-%d"), ".xlsx", sep = "")
+# 
+# #download the dataset from the website to a local temporary file
+# GET(url, authenticate(":", ":", type="ntlm"), 
+#     write_disk(tf <- tempfile(fileext = ".xlsx")))
+# 
+# #read the Dataset sheet into “R”
+# ecdc_data <- try(read_excel(tf), silent = TRUE)
+# 
 
-url <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",format(Sys.time(), "%Y-%m-%d"), ".xlsx", sep = "")
+# New version
 
-#download the dataset from the website to a local temporary file
-GET(url, authenticate(":", ":", type="ntlm"), 
-    write_disk(tf <- tempfile(fileext = ".xlsx")))
-
-#read the Dataset sheet into “R”
-ecdc_data <- try(read_excel(tf), silent = TRUE)
-
+#read the Dataset sheet into “R”. The dataset will be called "data".
+ecdc_data <- try(read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", 
+                          na.strings = "", fileEncoding = "UTF-8-BOM"),
+                 silent = TRUE)
