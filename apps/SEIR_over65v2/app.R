5f262648a3d2677c9c7e8a813cb51c6dce005ec5
Andrew Parnell <andrew.parnell@mu.ie>
2020-11-24 15:43:38 +0000

    Update vaccine app and explanation

--- apps/SEIR_over65v2/app.R
+++ apps/SEIR_over65v2/app.R
@@ -88,6 +88,11 @@ ui <- fluidPage(
                
                actionButton(inputId = "button", label = "Show/hide extra options"),
                
+               numericInput(inputId = "num_days",
+                            label = "Total number of days that vaccine can be administered",
+                            min = 0,
+                            value = 1000),
+               
                numericInput(inputId = "exp_Y",
                             label = "Number of asymptomatic spreaders under 65 at start date",
                             min = 0,
@@ -148,7 +153,7 @@ ui <- fluidPage(
                           fluidPage(
                             fluidRow(
                               
-                              p(HTML("<p> This visualisation shows the standard model of an epidemic where an individuals at any point in time can be in one of four states: S, susceptible to infection; E, exposed to the infection, and so infectious, but asymptomatic; I, infectious and symptomatic; R, recovered from the infection and immune to further infection. It is known as an SEIR model.<p> Exposed and Infectious people are the main actors in the system. They interact a random number of times each day with Susceptible, Exposed, Infectious, and Recovered people. The probability that a given interaction is with a Susceptible person is the fraction of people in the population that are Susceptible at that time. When they interact with a Susceptible person, the Susceptible person moves to being Exposed. An interaction with an Exposed, Infectious or Recovered person leads to no change in the system. We have extended this model to allow for two populations (here represented as under 65 or over 65) which can mix together at a set rate, which we call the 'Cross R number'.<p> Exposed people stay in that state for a random amount of time, with an average given by the model parameters, whereupon they become Infectious. Infectious people stay in that state for a random amount of time, with an average given by the model parameters, whereupon they become Recovered. Once there are no Exposed or Infectious people left, the epidemic has ended.<p> As the system is stochastic, significant variability occurs when the number of Exposed and Infectious people is small. When started with a small number of Exposed and Infectious people, there is a chance that the epidemic dies out before it can get going, or that it expands into a full-blown epidemic. Towards the end of a full blown epidemic, there is significant heterogeneity in the time until it ends. The closer the effective replicative value is to 1, the greater this variability. We have suppressed this variability in these plots, but they are available in some of other apps.<p> The vaccination aspect of the model is induced by instantly moving a set number of people from from state S to state R. This continues on a daily basis until there are no more people in the S category.<p> The forecasts produced by this system are inherently unrealistic. By creating such a prediction and presenting it to you makes this forecast less likely to happen. The government are likely to act, or people will react by themselves if there are large numbers of deaths.<p> The code presented here has been written by academics and not by professional coders. It may contain bugs or other mistakes which we have not disovered yet. All the code for this app is available in our <a href = 'https://github.com/hamilton-institute/covid19ireland'>GitHub</a> repository which we encourage you to look at and improve."))
+                              p(HTML("<p> This visualisation shows the standard model of an epidemic where an individuals at any point in time can be in one of four states: S, susceptible to infection; E, exposed to the infection, and so infectious, but asymptomatic; I, infectious and symptomatic; R, recovered from the infection and immune to further infection. It is known as an SEIR model.<p> Exposed and Infectious people are the main actors in the system. They interact a random number of times each day with Susceptible, Exposed, Infectious, and Recovered people. The probability that a given interaction is with a Susceptible person is the fraction of people in the population that are Susceptible at that time. When they interact with a Susceptible person, the Susceptible person moves to being Exposed. An interaction with an Exposed, Infectious or Recovered person leads to no change in the system. We have extended this model to allow for two populations (here represented as under 65 or over 65) which can mix together at a set rate, which we call the 'Cross R number'.<p> Exposed people stay in that state for a random amount of time, with an average given by the model parameters, whereupon they become Infectious. Infectious people stay in that state for a random amount of time, with an average given by the model parameters, whereupon they become Recovered. Once there are no Exposed or Infectious people left, the epidemic has ended.<p> As the system is stochastic, significant variability occurs when the number of Exposed and Infectious people is small. When started with a small number of Exposed and Infectious people, there is a chance that the epidemic dies out before it can get going, or that it expands into a full-blown epidemic. Towards the end of a full blown epidemic, there is significant heterogeneity in the time until it ends. The closer the effective replicative value is to 1, the greater this variability. We have suppressed this variability in these plots, but they are available in some of other apps.<p> The vaccination aspect of the model is induced by splitting up the Susceptible compartments into those awaiting vaccine, those who have been vaccinated but  are awaiting its effects, and those it has failed (either through refusal or through ineffectiveness). Similarly the Removed compartment is split up into those who have been removed due to having the disease, and those who have been removed due to being vaccinated.<p> The forecasts produced by this system are inherently unrealistic. By creating such a prediction and presenting it to you makes this forecast less likely to happen. The government are likely to act, or people will react by themselves if there are large numbers of deaths.<p> The code presented here has been written by academics and not by professional coders. It may contain bugs or other mistakes which we have not disovered yet. All the code for this app is available in our <a href = 'https://github.com/hamilton-institute/covid19ireland'>GitHub</a> repository which we encourage you to look at and improve."))
                               
                             )
                           )
@@ -167,6 +172,7 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   observeEvent(input$button, {
+    shinyjs::toggle("num_days")
     shinyjs::toggle("exp_Y")
     shinyjs::toggle("exp_O")
     shinyjs::toggle("inf_Y")
@@ -223,8 +229,8 @@ server <- function(input, output) {
         YR0O = input$R0_O_Y, 
         OR0Y = input$R0_O_Y, 
         OR0O = input$R0_O, 
-        Yvac = rep(input$vacc_Y, 10000),  
-        Ovac = rep(input$vacc_O, 10000), 
+        Yvac = rep(input$vacc_Y, input$num_days),  
+        Ovac = rep(input$vacc_O, input$num_days), 
         Veff = input$vacc_ef) %>% 
         as.data.frame %>% 
         rename("Time" = 1, "YSU" = 2, "YSV" = 3, 
